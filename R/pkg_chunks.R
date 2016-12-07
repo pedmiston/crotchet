424309eead3e1ab19a67af2e22b2481fcee602ad
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-06 21:22:43 -0600

    Include correct import in pkg chunk loader

--- R/pkg_chunks.R
+++ R/pkg_chunks.R
@@ -5,16 +5,17 @@ find_chunk_file <- function(name, package, inst_dir_name = "extdata") {
 
 #' @export
 read_all_pkg_chunk_files <- function(package) {
-  for (name in list_pkg_chunk_files(package)) {
+  for (name in list_pkg_chunk_files(package, strip_ext = TRUE)) {
     read_pkg_chunk_file(name, package)
   }
 }
 
 #' @export
-list_pkg_chunk_files <- function(package) {
-  list_extdata(package, re_files = "*.R", strip_ext = FALSE)
+list_pkg_chunk_files <- function(package, strip_ext = FALSE) {
+  list_extdata(package, re_files = "*.R", strip_ext = strip_ext)
 }
 
+#' @importFrom knitr read_chunk
 #' @export
 read_pkg_chunk_file <- function(name, package) {
   read_chunk(find_chunk_file(name, package))
